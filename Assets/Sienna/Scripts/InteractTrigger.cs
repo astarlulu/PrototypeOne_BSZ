98757e863251dfeb0084a33a5077517ca09b2e51
SiennaHa <sienna.hargreaves31@gmail.com>
2026-07-23 08:00:37 +1200

    /Sienna/InteractTrigger comments
    23/07/26

--- Assets/Sienna/Scripts/InteractTrigger.cs
+++ Assets/Sienna/Scripts/InteractTrigger.cs
@@ -3,35 +3,49 @@ using UnityEngine.InputSystem;
 
 public class InteractTrigger : MonoBehaviour
 {
-    //[SerializeField] private BoxCollider interactionCollider;
-    [SerializeField] private InputActionReference interactActionRef;
-    [SerializeField] private DialogueConversationsManager conversationScript;
-    [SerializeField] private SpriteRenderer exclamationSprite;
+    // keep all these references
+    [SerializeField] private InputActionReference interactActionRef; // input action
+    [SerializeField] private DialogueConversationsManager conversationScript; // conversation script reference
+    [SerializeField] private SpriteRenderer exclamationSprite; // exclamation point sprite
 
     private bool near = false; // check if the player is close enough
-    private InputAction interactAction; //for new unity system??
 
+    private InputAction interactAction; //for new unity system
 
+   
 
     private void Awake()
     {
-        interactAction = interactActionRef.action;
-        exclamationSprite.enabled = false;
+        interactAction = interactActionRef.action; // input stuff
+        exclamationSprite.enabled = false; // exclamation sprite off on awake
     }
 
 
+    
     public void Update()
     {
+        
+        
+        
+        // ------- can probably stay about the same? - just make "near" toggled by the ray elsewhere (or whatever works) - sienna
+        // if "near" is true, 'E' is pressed, and there isn't already a conversation playing
         if(near == true && interactAction.WasPressedThisFrame() && !conversationScript.conversationActive)
         {
             conversationScript.StartConversation();
         }
 
+        // turns off exclamation point
         if(conversationScript.conversationActive)
             exclamationSprite.enabled = false;
+        // ------
 
     }
 
+
+ // NOTE: I don't remember the raycast script but below here is probably the main thing to remove
+
+
+    // enter collider turns near true (replace with raycast)
     void OnTriggerEnter(Collider other)
     {
         
@@ -40,6 +54,7 @@ public class InteractTrigger : MonoBehaviour
         exclamationSprite.enabled = true;
     }
 
+    // exit collider turns near false (replace with raycast)
     void OnTriggerExit(Collider other)
     {
         
